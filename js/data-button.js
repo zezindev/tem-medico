4e9c4fe96df244fae4789df6bc30b3da35dd1514
Zé <joselucasskt@gmail.com>
2025-05-06 19:10:18 -0300

    Responsive layout

--- js/data-button.js
+++ js/data-button.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     const dateElement = document.querySelector(".date-text");
 
+    // Define e insere a data atual no elemento .date-text
     const options = { day: "numeric", month: "long" };
     const today = new Date().toLocaleDateString("pt-BR", options);
+    if (dateElement) {
+        dateElement.textContent = today;
+    }
 
-    dateElement.textContent = today; // Atualiza o texto com a data atual
+    // Hover do ícone de calendário
+    const menuDate = document.querySelector('.menu-date');
+    if (menuDate) {
+        const icon = menuDate.querySelector('.calendar-icon');
+
+        if (icon) {
+            menuDate.addEventListener('mouseenter', () => {
+                icon.src = 'assets/icons/calendar-hover.svg';
+            });
+
+            menuDate.addEventListener('mouseleave', () => {
+                icon.src = 'assets/icons/calendar.svg';
+            });
+        }
+    }
 });
+
+
+
